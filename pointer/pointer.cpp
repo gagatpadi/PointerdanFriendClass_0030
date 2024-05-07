26fc69c8f309c34ca7244e01938e1cac6b9d5af1
gagatpadi <padiyogaradhana@gmail.com>
2024-05-07 18:15:31 +0700

    part 2 class mahasiswa

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -5,21 +5,21 @@ class mahasiswa {
 		public:
 		int nim;
 		void showNim() {
-		cout << "No Induk = " << nim << endl;
+		cout << "No Induk = "<<nim<< endl;
 	}
 };
 
 int main() {
 	
-	mahasiswa mhs{1};
+	mahasiswa mhs{1}; // Object mhs
+	mhs.showNim();  // Member access operator
+
+	mahasiswa& refMhs = mhs; //pointer reference regmhs
+	refMhs.nim = 2; //member access operator
 	mhs.showNim();
 
-	mahasiswa& refMhs = mhs;
-	refMhs.nim = 2;
-	mhs.showNim();
-
-	mahasiswa* pMhs = &mhs;
-	pMhs->nim = 3;
+	mahasiswa* pMhs = &mhs; //pointer deference pmhs
+	pMhs->nim = 3; // arrow operator
 	pMhs->showNim();
 	return 0;
 
